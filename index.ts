2cb4666ecb440c77747e9d3b59efb18c7d299095
JIDE-73 <jimenezleonardo875@gmail.com>
2026-04-19 23:32:02 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Application } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
